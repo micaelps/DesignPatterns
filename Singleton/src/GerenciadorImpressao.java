da8f867ecd5f5983cf9b4932d857753aab955d99
micaelps <3.1416micael@gmail.com>
2021-01-11 13:12:04 -0300

    Singleton

---
+++ Singleton/src/GerenciadorImpressao.java
@@ -0,0 +1,16 @@
+public class GerenciadorImpressao {
+
+    private static  GerenciadorImpressao gerenciadorImpressao;
+
+    private GerenciadorImpressao(){
+
+    }
+
+    public static  GerenciadorImpressao getInstance(){
+        if (gerenciadorImpressao == null){
+            gerenciadorImpressao = new GerenciadorImpressao();
+        }
+        return  gerenciadorImpressao;
+    }
+
+}
